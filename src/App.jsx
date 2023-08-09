b84fe4f3b86d9ef3648d1fc679ea01eda0db1e1c
SDBoahen <SDBoahen@gmail.com>
2023-08-09 11:36:31 -0400

    Click Toggle Render Complete - Yaaay

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,18 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 import cohortArray from "./data/cohortArray.json"
+// This Retains ALL of the Fellows
 
 
 function App() {
 
-
   console.log("cohortArray: ", cohortArray)
-  // const [v, f]
 
-  const fellowsToRender = cohortArray.map( 
+
+  const [currentFellowsArray, setCurrentFellowsArray] = useState( cohortArray )
+  // currentFellowsArray = cohortArray
+
+  const fellowsToRender = currentFellowsArray.map( 
     (eachFellowObj)=>{  
       return(<div>
         <h4>Name: {eachFellowObj.name}</h4>
@@ -24,21 +27,44 @@ function App() {
 
 
   // Handling Duplicate Months
-  const filteredMonths = []
+  // const filteredMonths = []
   
-    cohortArray.forEach(  
-      (eachFellowObject)=>{
+  //   cohortArray.forEach(  
+  //     (eachFellowObject)=>{
 
-        // June July October March
-        if(eachFellowObject.birthdayMonth === "July"){
-          filteredMonths.push("July")
-        }
-      } 
-  )
-  console.log("filteredMonths: ", filteredMonths)
+  //       // June July October March
+  //       if(eachFellowObject.birthdayMonth === "July"){
+  //         filteredMonths.push("July")
+  //       }
+  //     } 
+  // )
+  // console.log("filteredMonths: ", filteredMonths)
 
-  const monthstoRender = filteredMonths.map( (eachMonthString) => <h2>{eachMonthString}</h2>    )
+  // const monthstoRender = filteredMonths.map( (eachMonthString) => <h2>{eachMonthString}</h2>    )
+  
+  const monthsRender = [
+    ...new Set(cohortArray.map((fellow) => fellow.birthdayMonth)),
+   ];  /// Brought to You By Mike K. ©
+   console.log(monthsRender)
 
+   const monthsInHTML = monthsRender.map( (eachMonthText)=>
+      // still retaining Arrow Function implicit return  
+      <h2 onClick={ (se) => handleMonthClick(se.target.innerText) }>
+        {eachMonthText}
+      </h2>
+    )
+
+
+    function handleMonthClick (clickedMonthText){
+      console.log(clickedMonthText)
+
+      // We are filtering from EVERYONE !!!!  ~  cohortArray (From JSON File)
+      const filteredFellows = cohortArray.filter( (eachFellow)=> eachFellow.birthdayMonth === clickedMonthText )
+
+      setCurrentFellowsArray( [...filteredFellows] )
+
+
+    }
 
 
 
@@ -59,7 +85,7 @@ function App() {
 
         <div className='birthday-month-list'>
           <h2>Birthday Months</h2>
-          {monthstoRender}
+          {monthsInHTML}
         </div>
 
 
